e0edc584dc7913e7caca2219574845abe9bea59d
Yuri Telis <128494725+yuritelis@users.noreply.github.com>
2025-08-18 10:55:00 -0300

    sétimo commit - edição de script

--- Assets/scripts/HpController.cs
+++ Assets/scripts/HpController.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class HpController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
